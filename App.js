9687f5e829d401e67da5cf2322c63a8597547db1
Matrix374 <rumahirfan@gmail.com>
2021-01-29 18:48:20 +0000

    Changed Formatting to JS Standard

--- App.js
+++ App.js
@@ -27,57 +27,16 @@ import {
 
 import LogInView from './src/components/views/logIn_view';
 
-class App extends Component{
-
-  render(){
+class App extends Component {
+  render() {
     return (
       <>
         <View>
-          <LogInView/>
+          <LogInView />
         </View>
-        
       </>
     );
   }
-  
-};
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+}
 
 export default App;
