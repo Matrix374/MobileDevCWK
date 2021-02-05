95836de7498411452346d23153182ced1692ecba
Matrix374 <rumahirfan@gmail.com>
2021-02-05 19:36:33 +0000

    Register Endpoint: Implemented POST Call

--- src/components/views/register_view.js
+++ src/components/views/register_view.js
@@ -6,11 +6,27 @@ export default class RegisterView extends Component {
     super(props);
 
     this.state = {
+      user: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+      },
+      firstName: '',
+      lastName: '',
       email: '',
       password: '',
+      success: false,
       buttonStyle: '#c79274',
     };
   }
+  handleFirstNameChange = (firstName) => {
+    this.setState({firstName: firstName});
+  };
+
+  handleLastNameChange = (lastName) => {
+    this.setState({lastName: lastName});
+  };
 
   handleEmailChange = (email) => {
     this.setState({email: email});
@@ -21,16 +37,63 @@ export default class RegisterView extends Component {
   };
 
   handleRegisterButtonClick = () => {
-    //do stuff
-    //If Email !Empty || Password !Empty throw Error
-    //Attempt POST Register (/user/)
-    ToastAndroid.show('Not Implemented', ToastAndroid.SHORT);
+    this.state.user = {
+      first_name: this.state.firstName,
+      last_name: this.state.lastName,
+      email: this.state.email,
+      password: this.state.password,
+    };
+    this.postRegister();
+
+    if(this.state.success)
+    {
+      this.props.navigation.navigate('LogIn');
+    }
   };
 
+  postRegister = async () => {
+    try {
+      let response = await fetch('http://10.0.2.2:3333/api/1.0.0/user/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.state.user),
+      });
+
+      if (response.ok) {
+        let json = await response.json();
+
+        console.log('json: ' + JSON.stringify(json));
+
+        this.setState({success: true})
+      } else {
+        ToastAndroid.show(response.status.toString(), ToastAndroid.SHORT);
+        throw new Error(response.status);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //Refactor Goals: Use Forms and Form-Bodies instead of this?
   render() {
     return (
       <View>
         <Text>COVFEFE REGISTER</Text>
+
+        <TextInput
+          placeholder="Enter First Name"
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={this.handleFirstNameChange}
+          value={this.state.firstName}
+        />
+        <TextInput
+          placeholder="Enter Last Name"
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={this.handleLastNameChange}
+          value={this.state.lastName}
+        />
 
         <TextInput
           placeholder="Enter Email"
