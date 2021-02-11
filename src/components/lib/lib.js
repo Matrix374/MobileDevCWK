b49f0a54c9f4959bd87a56c17e54f8ab1ac25481
Matrix374 <rumahirfan@gmail.com>
2021-02-11 08:19:55 +0000

    Fixed Lib to handle empty values

--- src/components/lib/lib.js
+++ src/components/lib/lib.js
@@ -1,35 +1,39 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default class Lib {
-
   retrieveToken = async () => {
     try {
       let json = await AsyncStorage.getItem('@user');
       let user = json != null ? JSON.parse(json) : null;
-      console.log('from lib, token retrieved: ' + user.token);
-      return user.token;
+
+      if (user?.token) {
+        console.log('from lib, token retrieved: ' + user.token);
+        return user.token;
+      } else return null;
     } catch (e) {
-      console.log("Token Retrieval Error: " + e.toString());
+      throw new Error('Token Retrieval Error: ' + e);
     }
   };
 
-  retrieveUserId = async() => {
+  retrieveUserId = async () => {
     try {
       let json = await AsyncStorage.getItem('@user');
       let user = json != null ? JSON.parse(json) : null;
-      console.log('from lib, id retrieved: ' + user.id);
-      return user.id;
+      if (user?.id) {
+        console.log('from lib, id retrieved: ' + user.id);
+        return user.id;
+      } else return null;
     } catch (e) {
-      console.log("ID Retrieval Error: " + e.toString());
+      throw new Error('ID Retrieval Error: ' + e);
     }
-  }
+  };
 
   DeleteUser = async () => {
     try {
       AsyncStorage.removeItem('@user');
       console.log('from lib, Deletion Success');
     } catch (e) {
-      console.log("User Data Deletion Error: " + e.toString());
+      throw new Error('User Data Deletion Error: ' + e);
     }
   };
 }
